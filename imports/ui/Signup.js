c283f9f7d0cbf366c96e6ee188fff5f0fdd40fec
matthova <matthova87@gmail.com>
2017-08-21 22:30:33 -0700

    Added create user functionality

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,11 +1,60 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import autobind from 'react-autobind';
+import { Accounts } from 'meteor/accounts-base';
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: '',
+    };
+
+    autobind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const email = this.email.value.trim();
+    const password = this.email.value.trim();
+
+    Accounts.createUser({ email, password }, (err) => {
+      console.log('signup callback', err);
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>Signup</p>
+        <p>Join Short Link</p>
+
+        {this.state.error
+          ? <p>
+            {this.state.error}
+          </p>
+          : null}
+
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="email"
+            ref={(email) => {
+              this.email = email;
+            }}
+            name="email"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            ref={(password) => {
+              this.password = password;
+            }}
+            name="password"
+            placeholder="Password"
+          />
+          <button>Create Account</button>
+        </form>
+
         <NavLink to="/">Already have an account?</NavLink>
       </div>
     );
