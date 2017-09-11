f55fa4e7fffd3bca5a45066edf7794c2cc45b43a
matthova <matthova87@gmail.com>
2017-09-11 10:15:35 -0700

    Added more TDD specs

--- tests/routes/test-privileges.js
+++ tests/routes/test-privileges.js
@@ -1,35 +1,62 @@
 /* global browser, describe, expect, it */
 describe('Login-logout functionality', () => {
-  it('should not be able to reach "/links" when not signed in', () => {
-    browser.url('http://localhost:3000');
-    console.log('eyyy', browser.getTitle());
-    expect(true).to.equal(false);
-  });
-  it('should be able to reach the login page when not signed in', () => {
-    expect(true).to.equal(false);
-  });
-  it('should be able to reach the signup page when not signed in', () => {
-    expect(true).to.equal(false);
-  });
   it('should be redirected to the "login" page when not signed in and visiting the "links" page', () => {
     expect(true).to.equal(false);
   });
-  it('should be able to reach the "links" page when signed in', () => {
+
+  it('should be able to reach the login page when not signed in', () => {
     expect(true).to.equal(false);
   });
+
+  it('should be able to reach the signup page when not signed in', () => {
+    expect(true).to.equal(false);
+  });
+
+  it('should be able to create a new user', () => {
+    expect(true).to.equal(false);
+  });
+
   it('should be redirected to the "links" page when signed in and vists the "login" page', () => {
     expect(true).to.equal(false);
   });
+
   it('should be redirected to the "links" page when signed in and visits the "signup" page', () => {
     expect(true).to.equal(false);
   });
-  it('should be able to reach the login page when not signed in', () => {
+
+  it('should be able to reach the "links" page when signed in', () => {
     expect(true).to.equal(false);
   });
-  it('should be able to reach the login page when not signed in', () => {
+
+  it('should be able to create a link', () => {
     expect(true).to.equal(false);
   });
-  it('should be able to reach the login page when not signed in', () => {
+
+  it('should be able to create a second link', () => {
+    expect(true).to.equal(false);
+  });
+
+  it('should be able to update a link', () => {
+    expect(true).to.equal(false);
+  });
+
+  it('should be able to delete a link', () => {
+    expect(true).to.equal(false);
+  });
+
+  it('should be able to log out', () => {
+    expect(true).to.equal(false);
+  });
+
+  it('should be able to log out', () => {
+    expect(true).to.equal(false);
+  });
+
+  it('should be able to create a second user', () => {
+    expect(true).to.equal(false);
+  });
+
+  it('the second user should not be able to see any link created by the first user', () => {
     expect(true).to.equal(false);
   });
 });
