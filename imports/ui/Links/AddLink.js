385d91f68a93d457ef8c551cdf5c5334f0585252
hovanem <matt.hova@autodesk.com>
2017-09-14 18:23:23 -0700

    Added ability to toggle visible links

--- imports/ui/Links/AddLink.js
+++ imports/ui/Links/AddLink.js
@@ -6,29 +6,39 @@ export default class AddLink extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      url: '',
+    };
+
     autobind(this);
   }
 
   createLink(e) {
     e.preventDefault();
 
-    const url = this.url.value.trim();
+    const { url } = this.state;
 
     if (url) {
       Meteor.call('links.insert', url, (error, response) => {
         if (error) {
           console.error(error);
         } else {
-          this.url.value = '';
+          this.setState({ url: '' });
         }
       });
     }
   }
 
+  onUrlChange(e) {
+    this.setState({
+      url: e.target.value,
+    });
+  }
+
   render() {
     return (
       <form onSubmit={this.createLink}>
-        <input type="text" ref={url => (this.url = url)} placeholder="URL" />
+        <input type="text" placeholder="URL" value={this.state.url} onChange={this.onUrlChange} />
         <input type="submit" value="Add Link" />
       </form>
     );
--- imports/ui/Links/LinkFilters.js
+++ imports/ui/Links/LinkFilters.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import { Tracker } from 'meteor/tracker';
 import { Session } from 'meteor/session';
 
-export default () => (
-  <div>
-    <label>
-      <input
-        type="checkbox"
-        onChange={(e) => {
-          Session.set('showVisible', e.target.checked);
-        }}
-      />
-      Show Hidden Links
-    </label>
-  </div>
-);
+export default class LinkFilters extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showVisible: true,
+    };
+  }
+
+  componentDidMount() {
+    this.visibleTracker = Tracker.autorun(() => {
+      this.setState({ showVisible: Session.get('showVisible') });
+    });
+  }
+
+  componentWillUnmount() {
+    this.visibleTracker.stop();
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={!this.state.showVisible}
+            onChange={(e) => {
+              Session.set('showVisible', !e.target.checked);
+            }}
+          />
+          Show Hidden Links
+        </label>
+      </div>
+    );
+  }
+}
